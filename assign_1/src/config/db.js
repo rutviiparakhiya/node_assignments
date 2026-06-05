626f421fc7d4c2a6e0f465d9573c767167eb84be
Rutvi <rparakhiya2007@gmail.com>
2026-06-05 09:20:56 +0530

    chore: remove env files and use default db connection strings

--- assign_1/src/config/db.js
+++ assign_1/src/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/notes-db";
+    const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
