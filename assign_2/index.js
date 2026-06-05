dc2e512c5008628f253fa3bd1875f05e752ebc39
Rutvi <rparakhiya2007@gmail.com>
2026-06-05 09:35:48 +0530

    fix: move index.js to root of each assignment for better deployment support

---
+++ assign_2/index.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
